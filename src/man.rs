f823ae39c1f5b14ca5ca92e6ab855e3da4a3abc4
Mikhail Katychev <mkatych@gmail.com>
2021-04-28 12:13:33 -0500

    added paging to dark man

--- src/man.rs
+++ src/man.rs
@@ -1,8 +1,9 @@
 use anyhow::{anyhow, Error};
 use argh::FromArgs;
 use mdcat::{push_tty, Environment, ResourceAccess, Settings, TerminalCapabilities, TerminalSize};
+use minus::{page_all, Pager};
 use pulldown_cmark::{Event, Options, Parser};
-use std::{io, str};
+use std::{fmt::Write, io, str};
 use syntect::parsing::SyntaxSet;
 
 const fn readme() -> &'static [u8] {
@@ -21,13 +22,33 @@ const fn reel() -> &'static [u8] {
     include_bytes!("../filmreel_md/reel.md")
 }
 
+const fn hidden_variables() -> &'static [u8] {
+    include_bytes!("../filmreel_md/extra_concepts/hidden_variables.md")
+}
+
+const fn ignored_variables() -> &'static [u8] {
+    include_bytes!("../filmreel_md/extra_concepts/ignored_variables.md")
+}
+
+const fn merge_cuts() -> &'static [u8] {
+    include_bytes!("../filmreel_md/extra_concepts/merge_cuts.md")
+}
+
+const fn retry_attempts() -> &'static [u8] {
+    include_bytes!("../filmreel_md/extra_concepts/retry_attempts.md")
+}
+
 /// Returns manual entries
 /// valid arguments are:
 ///
 /// readme,
 /// frame,
 /// cut,
-/// reel
+/// reel,
+/// hidden-variables,
+/// ignored-variables,
+/// merge-cuts,
+/// retry-attempts
 #[derive(FromArgs, PartialEq, Debug)]
 #[argh(subcommand, name = "man")]
 pub struct Man {
@@ -44,6 +65,10 @@ impl Man {
             "cut" => cut(),
             "reel" => reel(),
             "frame" => frame(),
+            "retry-attempts" => retry_attempts(),
+            "merge-cuts" => merge_cuts(),
+            "ignored-variables" => ignored_variables(),
+            "hidden-variables" => hidden_variables(),
             _ => {
                 return Err(anyhow!("invalid man argument: {}", &self.entry));
             }
@@ -82,7 +107,13 @@ impl Man {
                 .map_or_else(|| Err(anyhow!("termsize is None")), Ok)?,
         };
 
-        push_tty(settings, &env, &mut io::stdout(), parser)?;
+        let mut pager = Pager::new();
+        let mut buf = Vec::new();
+        push_tty(settings, &env, &mut buf, parser)?;
+        pager.lines = String::from_utf8(buf)?;
+        pager.prompt = "darkroom".to_string();
+
+        page_all(pager)?;
 
         Ok(())
     }
